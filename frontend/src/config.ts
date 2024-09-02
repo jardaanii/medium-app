8e5391c6b781bd329e43a18c72011ed764bbff5d
Akash <kiraacodes@gmail.com>
2024-09-02 15:40:10 +0530

    Added env variables

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://127.0.0.1:8787";
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "";
+export const BACKEND_URL = import.meta.env.BACKENDURL || "";
