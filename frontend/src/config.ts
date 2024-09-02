d0a0290e2ec46657b4adcd11785683c7af1c7539
Akash <kiraacodes@gmail.com>
2024-09-02 15:32:02 +0530

    added the log out nicely

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://127.0.0.1:8787";
-export const BACKEND_URL = "https://backend.akash19122017sf.workers.dev";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "";
