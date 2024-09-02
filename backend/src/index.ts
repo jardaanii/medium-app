1fba8bb6beea16013403613946a5f8b5e3184db7
Akash <kiraacodes@gmail.com>
2024-09-03 01:44:07 +0530

    Fixed cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import apiRoutes from "./routes/index";
 import { cors } from "hono/cors";
 const app = new Hono();
-app.use("/api/*", cors());
+app.use("*", cors());
 app.route("/api", apiRoutes);
 
 export default app;
